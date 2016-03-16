2b3e786a2df121bab0684366679774b3fd90e7d1
amhayslip <amhayslip@gmail.com>
2016-03-16 19:39:34 +0200

    add exercise

---
+++ main.js
@@ -0,0 +1,20 @@
+var coffeeShop = {
+  beans: 40,
+
+  drinkRequirements: {
+    latte: 10,
+    americano: 5,
+    doubleShot: 15,
+    frenchPress: 12
+  },
+
+  makeDrink: function (drinkType) {
+    // TODO: Finish this method
+  }
+}
+
+coffeeShop.makeDrink("latte");
+coffeeShop.makeDrink("americano");
+coffeeShop.makeDrink("filtered");
+coffeeShop.makeDrink("doubleShot");
+coffeeShop.makeDrink("frenchPress");
