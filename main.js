3a5579fc8ce7a710ff541bf0c2d3450f64ace0db
JonaCodes <jonathanfarache@gmail.com>
2018-08-18 09:20:01 +0300

    identifier change + expected output

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var coffeeShop = {
+const coffeeShop = {
   beans: 40,
 
   drinkRequirements: {
@@ -13,8 +13,8 @@ var coffeeShop = {
   }
 }
 
-coffeeShop.makeDrink("latte");
+coffeeShop.makeDrink("latte"); 
 coffeeShop.makeDrink("americano");
-coffeeShop.makeDrink("filtered");
+coffeeShop.makeDrink("filtered"); //should alert/console "Sorry, we don't make filtered"
 coffeeShop.makeDrink("doubleShot");
-coffeeShop.makeDrink("frenchPress");
+coffeeShop.makeDrink("frenchPress"); //should alert/console "Sorry, we're all out of beans"
